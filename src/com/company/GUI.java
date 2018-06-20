595bd485b53e0794db61d24cf0d8cf3f506327df
Erik <erikhansson85@gmail.com>
2018-06-20 09:40:09 +0200

    init

---
+++ src/com/company/GUI.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class GUI implements UI{
+
+    @Override
+    public void runGUI() {
+        Terminal terminal = null;
+        try {
+            terminal = new DefaultTerminalFactory().createTerminal();
+        } catch (IOException e) {
+            System.out.println("createTerminal error");
+            e.printStackTrace();
+        }
+        Screen screen = null;
+        try {
+            screen = new TerminalScreen(terminal);
+        } catch (IOException e) {
+            System.out.println("terminalScreen error");
+            e.printStackTrace();
+        }
+        try {
+            screen.startScreen();
+        } catch (IOException e) {
+            System.out.println("startScreen error");
+            e.printStackTrace();
+        }
+
+
+        /*
+        while(true){
+
+        screen.clear();
+
+        // Lägg in kod här..
+
+        TimeUnit.MILLISECONDS.sleep(33);
+        screen.refresh();
+        }
+         */
+
+    }
+}
